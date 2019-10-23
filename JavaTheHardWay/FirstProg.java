772a317a64a509c0bff6d2a47886a09a7eaf6763
Neal Savant <nealrsavant@gmail.com>
2019-10-23 15:36:22 -0400

    finished jthw ex 1

---
+++ JavaTheHardWay/FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg{
+    public static void main(String[] args){
+        System.out.println("I am going to become a programmer!");
+        System.out.println("This is exciting yes");
+    }
+}
